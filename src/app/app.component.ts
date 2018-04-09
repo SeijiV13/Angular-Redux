090b62e16e5de73657ef52c58facd7dcc0de6b88
seiji <villafrancasra@gmail.com>
2018-04-09 17:20:19 +0800

    Initial Redux Base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder} from '@angular/forms'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder){}
+  
+
+  ngOnInit(){
+   this.form = this.formBuilder.group({
+     username: [''],
+     password: ['']
+   })
+  }
+
+  onSubmit(){
+    console.log(this.form.controls['username'].value, this.form.controls['password'].value)
+  }
+ 
+
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+
+//REDUX
+import { StoreModule} from '@ngrx/store';
+import { EffectsModule} from '@ngrx/effects';
 
 
 @NgModule({
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    StoreModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
