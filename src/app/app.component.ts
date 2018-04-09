d7a9fe6680736e9de3f766cbf5edf0e2e97f8422
seiji <villafrancasra@gmail.com>
2018-04-09 17:22:14 +0800

    Remove Test codes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder} from '@angular/forms'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder){}
+
+  constructor(){}
   
 
   ngOnInit(){
-   this.form = this.formBuilder.group({
-     username: [''],
-     password: ['']
-   })
+
   }
 
   onSubmit(){
-    console.log(this.form.controls['username'].value, this.form.controls['password'].value)
   }
  
 
